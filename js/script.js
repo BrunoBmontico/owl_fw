38a8e269004e7e53813935d073a10a332a1bc555
bbonassi <bbonassi@lliege.com.br>
2025-10-06 14:00:26 -0300

    .

--- js/script.js
+++ js/script.js
@@ -1,37 +1,68 @@
-const { Component, mount, xml } = owl;
+const { Component, mount, xml, useRef, onMounted, useState } = owl;
 
 class Task extends Component {
     static template = xml`
     <div class="task" t-att-class="props.task.isCompleted ? 'done' : ''">
-        <input type="checkbox" t-att-checked="props.task.isCompleted"/>
+        <input type="checkbox" t-att-checked="props.task.isCompleted" t-on-click="toggleTask"/>
         <span><t t-esc="props.task.text"/></span>
-    </div>`;
+        <span class="delete" t-on-click="deleteTask">🗑</span>
+    </div>
+    `;
     
-    static props = ["task"];
+    toggleTask() {
+        this.props.task.isCompleted = !this.props.task.isCompleted
+    }
+
+    deleteTask() {
+        this.props.onDelete(this.props.task)
+    }
+
+    static props = ["task", "onDelete"];
 }
 
 class Root extends Component {
     static template = xml`
-    <div class="task-list">
-        <t t-foreach="tasks" t-as="task" t-key="task.id">
-            <Task task="task"/>
-        </t>
-    </div>`;
+    <div class="todo-app">
+        <input placeholder="Enter a new task" t-on-keyup="addTask" t-ref="add-input"/>
+        <div class="task-list">
+            <t t-foreach="tasks" t-as="task" t-key="task.id">
+                <Task task="task" onDelete.bind="deleteTask"/>
+            </t>
+        </div>
+    </div>
+    `;
 
     static components = { Task };
 
-    tasks = [
-        {
-            id: 1,
-            text: "buy milk",
-            isCompleted: true,
-        },
-        {
-            id: 2,
-            text: "clean house",
-            isCompleted: false,
-        },
-  ];
+    nextId = 1;
+    tasks = useState([]);
+  
+    addTask(ev) {
+        if (ev.keyCode === 13) {
+            const text = ev.target.value.trim();
+            ev.target.value = "";
+            
+            if (text) {
+                const newTask = {
+                    id: this.nextId++,
+                    text: text,
+                    isCompleted: false,
+                };
+                this.tasks.push(newTask);
+            }
+        }
+    }
+
+    deleteTask(task) {
+        const index = this.tasks.findIndex(t => t.id === task.id);
+        this.tasks.splice(index, 1)
+    }
+
+    setup() {
+        const inputRef = useRef("add-input");
+        onMounted(() => inputRef.el.focus());
+    }
+
 }
 
 mount(Root, document.body, {dev: true});
